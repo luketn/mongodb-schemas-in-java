e7580dc6abc1b8084397ccefe274f7421c66057a
luketn <endeavour9@gmail.com>
2025-08-02 15:38:00 +1000

    comment which is latitude which is longitude - I always forget!

--- src/main/java/com/luketn/seatemperature/datamodel/BoundingBox.java
+++ src/main/java/com/luketn/seatemperature/datamodel/BoundingBox.java
@@ -1,8 +1,10 @@
 package com.luketn.seatemperature.datamodel;
 
 public record BoundingBox(
+        // Latitude values for the bounds of the query
         Double north,
         Double south,
+        // Longitude values for the bounds of the query
         Double east,
         Double west
 ) { }
